b2848cefd5c344185aa043408744f20ba496c416
Taiye Ajimati <taiyeajimati@gmail.com>
2022-10-30 17:34:55 +0400

    Last office Commit

--- portfolio2/src/components/projects/Projects.jsx
+++ portfolio2/src/components/projects/Projects.jsx
@@ -1,77 +1,40 @@
 import React from 'react'
-import shot from '../../img/shot.svg'
-import Images from './project'
+import projects from './project'
 export default function Projects() {
 
   return (
-    <section className="experience min-h-[100vh] lg:h-[90vh] bg-primary  flex flex-col items-center pt-[4rem] lg:pt-[8rem] lg:px-[11rem]">
+    <section className="experience min-h-[100vh] lg:h-[90vh] bg-primary  flex flex-col items-center pt-[4rem] lg:pt-[8rem]">
       <h1 className='text-secondary lg:mb-[4rem] mb-[1.5rem] text-[1.5rem] lg:text-[2rem]  '>Palces I have worked</h1>
-      <div className="experience-wrapper border-[3px] rounded-xl px-6  border-opacity-[0.1] border-secondary  flex flex-col
+     
+      <div className="experience-wrapper border-[3px] rounded-xl px-6  border-opacity-[0.1] border-secondary 
        min-h-[400px] pb-[50px] container lg:py-[4rem] mt-[20px]
       ">
-        {/* {Images.map(item =>
-          <div key={item.id} className="test h-[290px] relative z-[500]">
+       
+        {projects.map(item =>
+        <div className="main flex">
+          <div className="project-list border flex-1 h-10">
 
-            <img src={item.pix} alt="" />
+          </div>
+         <div key={item.id} className="project-card h-[fit] relative
+         flex gap-[40px] justify-end bg-secondary  border-[yellow] border-4
+         ">
+            
+          <div  className="desc text-left">
+  <h1 className='text-[white] text-[1.8rem] '>{item.title}</h1>
+  <p className='text-[white]'>{item.desc}</p>
+  <p className='text-[white]'>{item.tools}</p>
+  </div>
+
+    <div className="img border-2 w-[400px]">
+            <img className='h-[300px] w-[100%]' src={item.pix} alt="" />
+            </div>
+
+          </div>
           </div>
 
-        )} */}
-        <div className="test h-[290px] relative z-[500]">
 
-          <img src="./" alt="" />
-        </div>
-
-
-
-        {/* <div className='tabs-head  text-[1.4rem] text-secondary 
-        border-b-[2px] flex   border-opacity-[0.1] border-secondary overflow-auto scrollbar-hide 
-        '     >
-          {
-            work.map((work, index) => {
-              return (
-
-                <div className={exp === index ? "after:rounded-[5px] px-[30px] py-[20px] relative bg-secondary bg-opacity-[.05] rounded-t-lg after:absolute after:bg-secondary after:left-0 after:right-0 after:bottom-[-2px] after:top-[97%] text-[1rem] lg:text-[1.2rem] cursor-pointer min-w-fit lg:mr-4" : "tab px-[30px] py-[20px]  cursor-pointer text-[1rem] lg:text-[1.2rem] min-w-fit lg:mr-8 "}
-                  onClick={() => activeTab(index)} key={index}
-                >{work.company}</div>
-
-              )
-
-            })
-          }
-        </div> */}
-
-
-        {/* <div className="tab-contents">
-          {
-            work.map((work, index) => {
-
-              return (
-
-                <div className={exp === index ? "content text-left lg:ml-[30px] pt-[30px]" : "tab hidden"}
-                  onClick={() => activeTab(index)} key={index} >
-                  <div className="top flex flex-col lg:flex-row mb-[40px] lg:mb-0  lg:items-center gap-4">
-                    <h1 className='text-[white] text-[1.5rem]'>{work.title}</h1>
-                    <h2 className='text-[1.5rem] text-green'>{work.year}</h2>
-                  </div>
-                  <span className='text-secondary opacity-40 pb-[30px]'>{work.about}</span>
-                  <ul className='space-y-2 mt-3'>
-                    <h1 className='text-green text-[1.2rem] '>Duties:</h1>
-                    {work.desc.map((duties, index) => (
-                      <li key={index} className='text-secondary flex items-center gap-2 space-y-4 '><span><svg width="18" height="12" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.35355 4.35355C9.54882 4.15829 9.54882 3.84171 9.35355 3.64645L6.17157 0.464466C5.97631 0.269204 5.65973 0.269204 5.46447 0.464466C5.2692 0.659728 5.2692 0.976311 5.46447 1.17157L8.29289 4L5.46447 6.82843C5.2692 7.02369 5.2692 7.34027 5.46447 7.53553C5.65973 7.7308 5.97631 7.7308 6.17157 7.53553L9.35355 4.35355ZM0 4.5H9V3.5H0V4.5Z" fill="#D6E869" />
-                      </svg>
-                      </span>{duties}</li>
-
-                    ))}
-                  </ul>
-                </div>
-
-
-              )
-
-            })
-          }
-        </div> */}
+        )}
+       
 
       </div>
 
--- portfolio2/src/components/projects/project.js
+++ portfolio2/src/components/projects/project.js
@@ -1,29 +1,29 @@
-import cap from '../../img/cap.png'
-import easy from '../../img/cap.png'
+import sass from '../../img/sass.jpg'
+import easy from '../../img/easy.jpg'
 
 
 const Images = [
     {
         id: 1,
-        title: "Digital Marketing Landing Page - ",
+        title: "Digital Marketing Landing Page ",
         desc: "2020-2023",
-        pix: "img/shot.svg",
+        pix: sass,
         tools: [
             "React.js",
             "TailwindCSS"
         ]
     },
 
-    {
-        id: 2,
-        title: "Personal Port - ",
-        desc: "2020-2023",
-        pix: "img/shot.svg",
-        tools: [
-            "React.js",
-            "TailwindCSS"
-        ]
-    }
+    // {
+    //     id: 2,
+    //     title: "Personal Port ",
+    //     desc: "2020-2023",
+    //     pix: easy,
+    //     tools: [
+    //         "React.js",
+    //         "TailwindCSS"
+    //     ]
+    // }
 
 ]
 
